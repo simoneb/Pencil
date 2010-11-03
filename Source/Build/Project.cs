248082bbf81eb5058d131bc731f375e09d3c27d8
Simone Busoli <simone.busoli@gmail.com>
2010-11-04 00:01:59 +0100

    Added R# annotations

--- Source/Build/Project.cs
+++ Source/Build/Project.cs
@@ -1,10 +1,11 @@
-using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Annotations;
 
 namespace Pencil.Build
 {
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
 	public class Project : IProject
 	{
 		readonly Dictionary<string, Target> targets;
--- make_new.bat
+++ make_new.bat
@@ -1,14 +1,18 @@
 @echo off
 color
+
 @Tools\Pencil.Build.exe Pencil_new.cs %*
-goto %ERRORLEVEL%
-rem Fail!, paint it red.
-:1
-	color 4F
-	goto done
+
+if ERRORLEVEL 1 goto error
+
 rem Success, paint it green.
-:0
 	color 2F
+	goto done
+:error
+rem Fail!, paint it red.
+	color 4F
+	
 :done
 
-pause
+@pause
+color
