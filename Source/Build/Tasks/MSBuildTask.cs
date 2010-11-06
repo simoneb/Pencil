cfdc534ca691ebb4d7ab5216c0a838774d944cbe
Simone Busoli <simone.busoli@gmail.com>
2010-11-06 18:57:08 +0100

    Minor fix

--- Source/Build/Tasks/MSBuildTask.cs
+++ Source/Build/Tasks/MSBuildTask.cs
@@ -24,7 +24,7 @@ namespace Pencil.Build.Tasks
 
         public MSBuildVerbosity? Verbosity { get; set; }
 
-        protected IDirectory FrameworksDirectory
+        private IDirectory FrameworksDirectory
         {
             get { return RuntimeDirectory.Parent; }
         }
